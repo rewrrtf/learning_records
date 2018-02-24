aef926a01ec8c6472de24c4351a0830115b14369
rewrrtf <rewrrtf@163.com>
2018-02-24 13:11:33 +0800

    git 下面第一个 C#程序

---
+++ csharp/ConsoleApplication/20180224/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20180224
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.Read();
+        }
+    }
+}
